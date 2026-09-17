625656243ca81549888b7641f3665dbb3b5523b8
Himanshu <himanshuchaudhary586@gmail.com>
2026-09-17 20:23:31 +0530

    version changed mcp to lower version, whole file code changed for that version

--- src/mcp_server/__init__.py
+++ src/mcp_server/__init__.py
@@ -1,56 +1,72 @@
-from fastmcp import FastMCP 
+from mcp.server.fastmcp import FastMCP
 import os
 from pymongo import MongoClient
-from default_category import default_categories 
+from default_category import default_categories
 import json
-mcp=FastMCP(name="expense-tracker")
+
+mcp = FastMCP(name="expense-tracker")
+
 
 def init_db():
-  URI=os.getenv("MONGO_URI")
-  client=MongoClient(URI)
-  db=client["Cluster0"]
-  collection=db["expense-tracker"]
-  return collection
+    URI = os.getenv("MONGO_URI")
+    client = MongoClient(URI)
+    db = client["Cluster0"]
+    collection = db["expense-tracker"]
+    return collection
 
-expenses=init_db()
 
-@mcp.tool
-def add(expense:int,spent_on:str,date:str,time:str):
-   expenditure={
-      "expense":expense,
-      "spent_on":spent_on,
-      "date":date,
-      "time":time
-   }
-   result=expenses.insert_one(expenditure)
+expenses = init_db()
 
-   if result.inserted_id:
-    return f"result :{result.inserted_id}"
-   else:
-    return "not done"
 
-@mcp.tool
-def show(date:str="",time:str="",start_date:str="",end_date:str=""):
-  query={}
-  
-  if start_date and end_date:
-    query["date"]={"$gte":start_date,"$lte":end_date}
-  elif date:
-    query["date"]=date
+@mcp.tool()
+def add(expense: int, spent_on: str, date: str, time: str):
+    expenditure = {
+        "expense": expense,
+        "spent_on": spent_on,
+        "date": date,
+        "time": time
+    }
 
-  if time:
-    query["time"]=time
+    result = expenses.insert_one(expenditure)
 
-  result=list(expenses.find(query))
+    if result.inserted_id:
+        return f"result: {result.inserted_id}"
+    else:
+        return "not done"
 
-  for r in result:
-    r["_id"]=str(r["_id"])
-  
-  return result
-  
+
+@mcp.tool()
+def show(
+    date: str = "",
+    time: str = "",
+    start_date: str = "",
+    end_date: str = ""
+):
+    query = {}
+
+    if start_date and end_date:
+        query["date"] = {
+            "$gte": start_date,
+            "$lte": end_date
+        }
+    elif date:
+        query["date"] = date
+
+    if time:
+        query["time"] = time
+
+    result = list(expenses.find(query))
+
+    for r in result:
+        r["_id"] = str(r["_id"])
+
+    return result
+
+
 @mcp.resource("category://list")
 def categories():
-  return json.dumps(default_categories)
- 
-if __name__=="__main__":
-    mcp.run()
+    return json.dumps(default_categories)
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
